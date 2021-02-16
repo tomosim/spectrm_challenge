0ace24cd0d3a9e920868a6d9a97ea0baade791ad
Tom Simmons <tom_simmons@live.co.uk>
2021-02-16 11:16:38 +0100

    generate uuids by default

--- db/migrations/20210216103325_messages.js
+++ db/migrations/20210216103325_messages.js
@@ -1,11 +1,15 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("messages", (table) => {
-    table.uuid("ID");
-    table.string("content", 255);
-    table.integer("retrieval_count").defaultTo(0);
+  return knex.raw('create extension if not exists "uuid-ossp"').then(() => {
+    return knex.schema.createTable("messages", (table) => {
+      table.uuid("id").defaultTo(knex.raw("uuid_generate_v4()"));
+      table.string("content", 255);
+      table.integer("retrieval_count").defaultTo(0);
+    });
   });
 };
 
 exports.down = function (knex) {
-  knex.schema.dropTable("messages");
+  return knex.schema.dropTable("messages").then(() => {
+    return knex.raw('drop extension if exists "uuid-ossp"');
+  });
 };
