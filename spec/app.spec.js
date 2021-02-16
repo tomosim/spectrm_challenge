eaf5d246a39a940774f7aadd54dc966133ae3fee
Tom Simmons <tom_simmons@live.co.uk>
2021-02-16 11:46:49 +0100

    add first test

---
+++ spec/app.spec.js
@@ -0,0 +1,50 @@
+process.env.NODE_ENV = "test";
+
+const { expect } = require("chai");
+const request = require("supertest");
+const app = require("../app");
+
+describe("/api", () => {
+  describe("/messages", () => {
+    describe("GET", () => {
+      it("200 - responds with an array of message objects", () => {
+        return request(app)
+          .get("/api/messages")
+          .expect(200)
+          .then((res) => {
+            res.body.messages.forEach((message) => {
+              expect(message).to.have.all.keys([
+                "id",
+                "content",
+                "retrieval_count",
+              ]);
+            });
+          });
+      });
+    });
+  });
+  describe("/message/:id", () => {
+    describe("GET", () => {
+      // ERRORS
+      // 404 id not found
+      // 400 not uuid
+    });
+    describe("POST", () => {
+      // ERRORS
+      // 400 contains HTML
+      // 400 too long
+    });
+    describe("PATCH", () => {
+      // ERRORS
+      // 404 id not found
+      // 400 not uuid
+      // 400 contains HTML
+      // 400 too long
+    });
+    describe("DELETE", () => {
+      // ERRORS
+      // 404 id not found
+      // 400 not uuid
+    });
+  });
+});
